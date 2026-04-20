36529bd4470aecd995e5a8b931cf9a3150b17f46
Quin F. <cheesits456@haileybot.com>
2026-04-20 10:03:19 -0700

    Add variable to store the base64 alphabet

--- base64-converter.js
+++ base64-converter.js
@@ -1,5 +1,6 @@
 const action = process.argv[2];
 const input = process.argv.slice(3).join(" ");
+const base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 
 if (!["encode", "decode"].includes(action)) return console.log("Error: First arg must be one of 'encode' or 'decode'");
 
